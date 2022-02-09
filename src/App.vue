334735fd724c74c52e18fb924483e6843c4fe0f2
Vendy Prum <vendy@thinkful.com>
2022-02-09 14:25:37 -0800

    able to toggle task and strikes out the text upon clicking

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@
    <button>Submit</button>
  </form>
  <ul>
-  <li :key="todo.id" v-for="todo in toDos">
-    <h3>{{todo.content}}</h3>
+  <li  v-for="toDo in toDos" :key="toDo.id" class="toDo">
+    <h3 :class="{ done: toDo.done }" @click="toggleDone(toDo)">{{toDo.content}}</h3>
   </li>
+
  </ul>
 </template>
 
@@ -23,9 +24,14 @@ export default {
     const toDos = ref([])
 
     function addNewTodo(){
-      toDos.value.push({id: Date.now(), done:false, content: newTodo.value})
+      toDos.value.push({id: Date.now(), done: false, content: newTodo.value})
+      newTodo.value = ''
+    }
+    function toggleDone(toDo){
+      toDo.done = !toDo.done
     }
     return {
+      toggleDone,
       toDos,
       newTodo,
       addNewTodo
@@ -43,5 +49,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.toDo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
